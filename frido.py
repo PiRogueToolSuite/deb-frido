7df1645a4b5bb0fa04e44db2fa9fae0da391b317
Cyril Brulebois <cyril@debamax.com>
2024-06-24 00:13:40 +0200

    Reorganize and document all options.

--- frido.py
+++ frido.py
@@ -21,14 +21,26 @@ STATE_FILE = Path('state.yaml')
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser(description='Frida auto-packager')
-    parser.add_argument('--refresh', action='store_true')
-    parser.add_argument('--refresh-git', action='store_true')
-    parser.add_argument('--refresh-reference', action='store_true')
-    parser.add_argument('--build', action='store_true')
-    parser.add_argument('--cheat', action='store_true')
-    parser.add_argument('--only-one', action='store_true')
-    parser.add_argument('--no-notify', action='store_true')
-    parser.add_argument('--no-fetch', action='store_true')
+
+    actions = parser.add_argument_group('main actions')
+    actions.add_argument('--refresh', action='store_true',
+                         help='refresh both git data and reference files')
+    actions.add_argument('--refresh-git', action='store_true',
+                         help='refresh git data')
+    actions.add_argument('--refresh-reference', action='store_true',
+                         help='refresh reference files')
+    actions.add_argument('--build', action='store_true',
+                         help='build versions listed as todo')
+
+    options = parser.add_argument_group('fine-tuning options')
+    options.add_argument('--only-one', action='store_true',
+                         help='restrict building to a single version')
+    options.add_argument('--cheat', action='store_true',
+                         help='skip building, reuse existing build results instead')
+    options.add_argument('--no-fetch', action='store_true',
+                         help='skip "git fetch" when refreshing git data')
+    options.add_argument('--no-notify', action='store_true',
+                         help='skip notifying the Discord channel')
     args = parser.parse_args()
 
     # We have 3 options for granularity:
